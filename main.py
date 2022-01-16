4e92a8389ccfed686e9cef5837270500b627d53f
Hudrolax <hudro795@gmail.com>
2022-01-16 12:40:34 +0300

    bugfix

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+
 import pandas.io.sql
 import requests
 from bs4 import BeautifulSoup
@@ -9,11 +11,12 @@ import pandas as pd
 import sqlite3
 from time import sleep
 from config import NEWS_URL, UNI_TELEBOT_URL
+from typing import Union
 
 sys.setrecursionlimit(10000)
 
 
-def get_delist_rec(_obj, _news):
+def get_delist_recursion(_obj: Union[dict, list], _news: list) -> None:
     if isinstance(_obj, dict):
         if 'title' in _obj.keys() and (
                 _obj.get('title').lower().find('delist') > 0 or _obj.get('title').lower().find('делист') > 0):
@@ -24,25 +27,30 @@ def get_delist_rec(_obj, _news):
         else:
             for key in _obj.keys():
                 child = _obj.get(key)
-                get_delist_rec(child, _news)
+                get_delist_recursion(child, _news)
     elif isinstance(_obj, list):
         for child in _obj:
-            get_delist_rec(child, _news)
+            get_delist_recursion(child, _news)
 
 
-def get_binance_news():
+def get_binance_news() -> Union[pd.DataFrame, None]:
+    _logger = logging.getLogger('get_binance_news')
     url = NEWS_URL
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
+    except requests.exceptions.ConnectionError as ex:
+        _logger.error(ex)
+        return None
     soup = BeautifulSoup(response.text, 'lxml')
 
     script = soup.find('script', {'id': '__APP_DATA'}).text
     data = json.loads(script)
     news = []
-    get_delist_rec(data, news)
+    get_delist_recursion(data, news)
     return pd.DataFrame(news)
 
 
-def send_to_telegram(df):
+def send_to_telegram(df: pd.DataFrame) -> None:
     if not df.empty:
         pprint(df)
         text = 'Новости Binance:\n'
@@ -58,8 +66,12 @@ def send_to_telegram(df):
 
 
 if __name__ == '__main__':
+    logger = logging.getLogger('main')
     while True:
         df = get_binance_news()
+        if df is None:
+            sleep(60)
+            continue
         con = sqlite3.connect('news.db')
         try:
             df_sql = pd.read_sql('SELECT * FROM news', con)
